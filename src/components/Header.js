0e894faf390240c7ad752f5473e334afe71a7ab6
Tom <tom.s.jackson090@gmail.com>
2020-12-10 15:05:48 +0000

    feat: adding assets to UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-import { CardContent, Paper, Typography } from "@material-ui/core";
+import { Card, Paper, Typography } from "@material-ui/core";
 import React from "react";
+import hero from "../media/hero.mp4";
+import styles from "../CSS/header.module.css";
 
 export default function Header() {
   return (
     <React.Fragment>
       <Paper>
-        <CardContent>
-          <Typography variant="h1">Receipt Reader</Typography>
+        <Card>
+          <video src={hero} style={styles.vid} autoPlay={true}></video>
           <Typography variant="p">
-            Upload an image of a receipt then we'll read it for you and adjust
-            the values to reflect tax it's eligible for.
+            Upload an image of a receipt, then we'll read it for you and adjust
+            the values to include any tax the items are eligible for.
           </Typography>
-        </CardContent>
+        </Card>
       </Paper>
     </React.Fragment>
   );
