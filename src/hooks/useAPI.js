13ec4a6107ae855bf108645af8484d8af9a0f596
Tom <tom.s.jackson090@gmail.com>
2020-12-27 12:36:41 +0000

    working call to cloudvision API

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,8 +1,49 @@
 const useAPI = () => {
-  const sendFiles = (files) => {
-    return "hello world"
+  const { GOOGLE_CLOUD_VISION_API_KEY } = process.env;
+  const sendFiles = async (files) => {
+    try {
+      let { image } = files;
+      let body = JSON.stringify({
+        requests: [
+          {
+            features: [
+              { type: "LABEL_DETECTION", maxResults: 10 },
+              { type: "LANDMARK_DETECTION", maxResults: 5 },
+              { type: "FACE_DETECTION", maxResults: 5 },
+              { type: "LOGO_DETECTION", maxResults: 5 },
+              { type: "TEXT_DETECTION", maxResults: 5 },
+              { type: "DOCUMENT_TEXT_DETECTION", maxResults: 5 },
+              { type: "SAFE_SEARCH_DETECTION", maxResults: 5 },
+              { type: "IMAGE_PROPERTIES", maxResults: 5 },
+              { type: "CROP_HINTS", maxResults: 5 },
+              { type: "WEB_DETECTION", maxResults: 5 },
+            ],
+            image: {
+              source: {
+                imageUri: image,
+              },
+            },
+          },
+        ],
+      });
+      let response = await fetch(
+        "https://vision.googleapis.com/v1/images:annotate?key=" +
+          "AIzaSyBwMZrlq-FRCJK52Qzy_8R6C0zzDOlGL1E",
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: body,
+        }
+      );
+      let responseJson = await response.json();
+      console.log(responseJson);
+    } catch (error) {
+      console.log(error);
+    }
   };
-
   return { sendFiles };
 };
 
